16af07b522edb29b87b1bf85ccb7c858ccabb955
Adrian Grajeda <joseadriangrajedahurtado@gmail.com>
2025-03-13 10:05:50 -0700

    se configuro la ip de produccion

--- config/conectionBD.js
+++ config/conectionBD.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
     user: 'postgres',//process.env.DB_USER,
-    host: 'localhost',//process.env.DB_HOST,
+    //host: '10.21.64.101',//process.env.DB_HOST,
+    host: '127.0.0.1',//process.env.DB_HOST,
+    //database: 'BD_CEF',//process.env.DB_NAME,
     database: 'postgres',//process.env.DB_NAME,
     password: '3ddn0!',//process.env.DB_PASSWORD,
     port: 5432,//process.env.DB_PORT

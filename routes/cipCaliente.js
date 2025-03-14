ce19efb4e0048365f4aaa4a38a581765cde7f650
Adrian Grajeda <joseadriangrajedahurtado@gmail.com>
2025-03-13 18:03:34 -0700

    se añadio una consulta para ver la duracion de un estado, cuando inicio y cunado acabo

--- routes/cipCaliente.js
+++ routes/cipCaliente.js
@@ -87,4 +87,46 @@ router.use('/operacion', async (req, res) => {
     }
 });
 
+// /api/caliente/operacion?fechaInicio=2025-03-10&fechaFin=2025-03-10
+router.use('/operacionTiempo', async (req, res) => {
+
+    const fechaI = req.query.fechaInicio;
+    const fechaF = req.query.fechaFin;
+
+    try {
+        const query = `
+           WITH numerado AS (
+            SELECT 
+                "Operacion", 
+                "Fecha",
+                row_number() OVER (ORDER BY "Fecha") AS rn,
+                row_number() OVER (PARTITION BY "Operacion" ORDER BY "Fecha") AS rn_op
+            FROM "Coca-cola"."tblCipFrio"
+            WHERE "Fecha" BETWEEN '${fechaI} 00:00:00' AND '${fechaF} 23:59:59'
+            ),
+            agrupado AS (
+                SELECT 
+                    "Operacion", 
+                    "Fecha", 
+                    rn - rn_op AS grupo
+                FROM numerado
+            )
+            SELECT 
+                "Operacion", 
+                COUNT(*) AS cantidad,
+                MIN("Fecha") AS inicio,
+                MAX("Fecha") AS fin
+            FROM agrupado
+            GROUP BY "Operacion", grupo
+            ORDER BY inicio;
+        `;
+        
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (error) {
+        console.error('Error en la consulta:', error);
+        res.status(500).json({ error: 'Error al obtener las temperaturas' });
+    }
+});
+
 module.exports = router;

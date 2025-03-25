5d510d2c162b003040ccd3a5b73074131f399267
Adrian Grajeda <joseadriangrajedahurtado@gmail.com>
2025-03-25 12:08:08 -0700

    cambios pequeños en la ip, para que este lista para produccio y pruebas

--- config/conectionBD.js
+++ config/conectionBD.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const pool = new Pool({
     user: 'postgres',//process.env.DB_USER,
-    //host: '10.21.64.101',//process.env.DB_HOST,
-    host: '127.0.0.1',//process.env.DB_HOST,
-    //database: 'BD_CEF',//process.env.DB_NAME,
-    database: 'postgres',//process.env.DB_NAME,
+    host: '10.21.64.160',//process.env.DB_HOST,
+    //host: '127.0.0.1',//process.env.DB_HOST,
+    database: 'DB_CEF',//process.env.DB_NAME,
+    //database: 'postgres',//process.env.DB_NAME,
     password: '3ddn0!',//process.env.DB_PASSWORD,
     port: 5432,//process.env.DB_PORT
 });
